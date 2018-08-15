66ba11658f683ae4c3004298e8aa5be404b8efb8
johnwoodill <johnwoodill@gmail.com>
2018-08-14 21:48:39 -0600

    working on RMSE for lm and nn

--- build_data.py
+++ build_data.py
@@ -14,9 +14,27 @@ dd['year'] = dd['year'].astype(int)
 dd['fips'] = dd['fips'].astype(str)
 dd['year'] = dd['year'].astype(str)
 
+# Degree days
+dday = pd.read_stata('/Users/john/Projects/corn_yield_pred/data/FULL_ddayByYearandFips_cropAreaWeighted.dta')
+dday['dday0_10C'] = dday['dday0C'] - dday['dday10C']
+dday['dday10_30C'] = dday['dday10C'] - dday['dday30C']
+dday['fips'] = dday['fips'].astype(int)
+dday['year'] = dday['year'].astype(int)
+dday['fips'] = dday['fips'].astype(str)
+dday['year'] = dday['year'].astype(str)
 
+# Precipitation
+#prec = pd.read_csv('/Users/john/Projects/corn_yield_pred/data/fips_precipitation_1900-2013.csv')
+#prec = prec[prec['month'] >= 3]
+#prec = prec[prec['month'] <= 10]
+#prec = prec.groupby(['fips', 'year'])['ppt'].sum()
+#prec = prec.reset_index()
+#prec['fips'] = prec['fips'].astype(str)
+#prec['year'] = prec['year'].astype(str)
+#prec['prec_sq'] = prec['ppt']**2
+#prec.columns = ['fips', 'year', 'prec', 'prec_sq']
 
-
+# Corn data
 cropdat = pd.read_csv('/Users/john/Projects/corn_yield_pred/data/corn_io_il_in.csv')
 cropdat = cropdat.dropna()
 cropdat['county_ansi'] = cropdat.county_ansi.astype(int)
@@ -31,12 +49,25 @@ cropdat.columns = ['year', 'fips', 'corn_acres', 'corn_yield']
 cropdat['fips'] = cropdat['fips'].astype(str)
 cropdat['year'] = cropdat['year'].astype(str)
 
-cropdat = cropdat.merge(dd, how = 'left', on = ['fips', 'year'])
-cropdat = cropdat.dropna()
+# Merge data
+#cropdat = cropdat.merge(dd, how = 'left', on = ['fips', 'year'])
+#cropdat = cropdat.dropna()
+
+
+# Merge Wolfram data
+cropdat = cropdat.merge(dday, how = 'left', on = ['fips', 'year'])
 
 # Trend
 cropdat['trend'] = cropdat['year'].astype(int) - 1979
 cropdat['trend_sq'] = cropdat['trend']**2
 
+# Quad precipitation
+cropdat['prec_sq'] = cropdat['prec']**2
+
+cropdat['ln_corn_yield'] = np.log(1 + cropdat.corn_yield)
+
+cropdat = cropdat.dropna()
 cropdat.to_pickle('/Users/john/Projects/corn_yield_pred/data/full_data.pickle')
 cropdat = pd.read_pickle('/Users/john/Projects/corn_yield_pred/data/full_data.pickle')
+
+print(cropdat.head())
