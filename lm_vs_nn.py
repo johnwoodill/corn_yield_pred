be862e04d4bb4abbfabf8149442408def1caff49
johnwoodill <johnwoodill@gmail.com>
2018-08-15 07:33:06 -0600

    finished model functions

--- lm_vs_nn.py
+++ lm_vs_nn.py
@@ -16,42 +16,14 @@ from keras.layers import Input, Dense
 from keras.models import Model, Sequential
 from sklearn.preprocessing import StandardScaler
 
-# Demean variables
-def demean_values(data, fe):
-	'''
-	Demean fixed-effect (fe) from values
-    ...
-    Arguments
-    ---------
-    data             : data to demean
-                      Flat array or list with dependent variable
-    fe               : fixed-effect from data
-   
-    -------
-    yxd               : returned demean values
-	'''
-	yx = pd.DataFrame(data)
-	yx['fe'] = fe
-	demeaner1 = yx.groupby('fe').mean()
-	demeaner1 = yx[['fe']].join(demeaner1, on='fe')\
-    	    .drop(['fe'], axis=1)
-	yx = yx.drop(['fe'], axis=1)
-	yxd = yx - demeaner1	
-	return yxd
-
-# Leave-one-out cross-validation
-def loo_cv(mod, data):
-	for i in range(pd.to_numeric(data.trend)):
-		train = data[data.trend != i]
-
 # Estimate WLS and return RMSE
 def felm_rmse(y, X, weights, X_test, y_test):
     mod = sm.WLS(y, X, weights = weights).fit()
     pred = mod.predict(X_test)
-    res = (np.array(y_test) - np.array(pred))**2
-    return np.sqrt(np.mean(res))
+    res = (y_test.ln_corn_yield.values - pred.values)
+    return np.sqrt(np.mean(res**2))
     
-def nnetwork_rmse(y, X, test_data):
+def nnetwork_rmse(y, X, X_test, y_test):
     # Build sequential model
     ksmod = Sequential()
     ksmod.add(Dense(12, input_dim=X.shape[1], activation='relu'))
@@ -62,12 +34,17 @@ def nnetwork_rmse(y, X, test_data):
     
     # Fit model
     ksmod.fit(X, y, epochs=10, batch_size = 20)
-    nn_pred = ksmod.predict(test_data)
+    nn_pred = ksmod.predict(X_test)
     
     # Get RMSE
-    nn_res = (np.array(y[['ln_corn_yield']]) - np.array(nn_pred))**2
-    return np.sqrt(np.mean(nn_res))
+    nn_res = (y_test - nn_pred)
+    return np.sqrt(np.mean(nn_res**2))
 
+# Leave-one-out cross-validation
+def loo_cv(mod, data):
+	for i in range(pd.to_numeric(data.trend)):
+		train = data[data.trend != i]
+		
 
 # Load data from build_data.py
 cropdat = pd.read_pickle('/Users/john/Projects/corn_yield_pred/data/full_data.pickle')
@@ -93,7 +70,7 @@ fe = pd.get_dummies(cropdat.fips)
 X = pd.DataFrame(np.hstack((X, fe)))
 
 # Get RMSE
-ddreg_rmse = felm_rmse(y, X, weights = cropdat[['corn_acres']], test_data = X)
+ddreg_rmse = felm_rmse(y, X, weights = cropdat[['corn_acres']], X_test = X, y_test = y)
 ddreg_rmse
 
 lfe_rrmse = (ddreg_rmse - baseline_rmse)/ddreg_rmse
@@ -110,29 +87,11 @@ X = X_scale.transform(X)
 
 y = np.array(y).reshape(-1, 1)
 
-nn_rmse = nnetwork_rmse(y, X, test_data = X)
-
-# Build sequential model
-ksmod = Sequential()
-ksmod.add(Dense(12, input_dim=X.shape[1], activation='relu'))
-ksmod.add(Dense(8, activation='relu'))
-ksmod.add(Dense(4, activation='relu'))
-ksmod.add(Dense(1, activation='relu'))
-ksmod.compile(optimizer='adam', loss='mean_squared_error', metrics=['mse'])
-
-# Fit model
-ksmod.fit(X, y, epochs=50, batch_size = 32)
-nn_pred = ksmod.predict(test)
-
-# Get RMSE
-nn_errors = y - nn_pred
-nn_rmse = np.sqrt(np.mean(nn_errors**2))
+nn_rmse = nnetwork_rmse(y, X, X_test = X, y_test = y)
 nn_rrmse = (nn_rmse - baseline_rmse)/baseline_rmse
-nn_rrmse
-
-rrmse = np.array([lfe_rrmse*-1, nn_rrmse*-1])
 
 # Plot
+rrmse = np.array([lfe_rrmse*-1, nn_rrmse*-1])
 plt.rcdefaults()
 fig, ax = plt.subplots()
 ax = ax.bar(['FE Model', 'NN Model'], rrmse)
